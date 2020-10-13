8446a45a15899def1bd1e05a568dad7663a8d54b
adrani_laptop <riadh_adrani@hotmail.fr>
2020-10-13 16:24:37 +0100

    added a floating action button adding a dummy note as an action

--- app/src/main/java/com/example/noter/MainActivity.java
+++ app/src/main/java/com/example/noter/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.noter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -15,21 +22,62 @@ public class MainActivity extends Activity {
     RecyclerView.Adapter mAdapter;
     RecyclerView.LayoutManager mLayoutManager;
 
+    FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_layout);
 
-        exampleCategory.add(new Note("note one",R.drawable.icon_big,"one"));
-        exampleCategory.add(new Note("note two",R.drawable.icon_big,"two"));
-        exampleCategory.add(new Note("note three",R.drawable.icon_big,"three"));
+        fab = findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNote();
+            }
+        });
 
+
+        useDummyElements();
+        buildRecyclerView();
+
+    }
+
+    void useDummyElements(){
+        // Testing the RecyclerView
+        // Dummy element ahead
+        exampleCategory.add(new Note("note 1",R.drawable.icon_big,"one"));
+        exampleCategory.add(new Note("note 2",R.drawable.icon_big,"two"));
+        exampleCategory.add(new Note("note 3",R.drawable.icon_big,"three"));
+        exampleCategory.add(new Note("note 4",R.drawable.icon_big,"four"));
+        exampleCategory.add(new Note("note 5",R.drawable.icon_big,"five"));
+        exampleCategory.add(new Note("note 6",R.drawable.icon_big,"six"));
+        exampleCategory.add(new Note("note 7",R.drawable.icon_big,"seven"));
+        exampleCategory.add(new Note("note 8",R.drawable.icon_big,"eight"));
+        exampleCategory.add(new Note("note 9",R.drawable.icon_big,"nine"));
+        exampleCategory.add(new Note("note 10",R.drawable.icon_big,"ten"));
+        exampleCategory.add(new Note("note 11",R.drawable.icon_big,"eleven"));
+        exampleCategory.add(new Note("note 12",R.drawable.icon_big,"twelve"));
+    }
+
+    void buildRecyclerView(){
         mRecyclerView = findViewById(R.id.category_RecyclerView);
-        // mRecyclerView.setHasFixedSize(true);
+
+        // If the list contains an always fixed number of element,
+        // set the boolean in the next line of code to TRUE
+        // In the case of this note app, the size may vary by deleting/adding new notes
+        // should be set to FALSE (if not using dummy data)
+        mRecyclerView.setHasFixedSize(false);
+
         mLayoutManager = new LinearLayoutManager(this);
         mAdapter = new NoteAdapter(exampleCategory);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    void addNote(){
+        exampleCategory.add(0,new Note("new Note ("+ (int) (Math.random()*10) +")",R.drawable.icon_big,"new_note"));
+        buildRecyclerView();
     }
 }
